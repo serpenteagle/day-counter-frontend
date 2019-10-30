5201e2d930525d49ca4526d283bb14bf4bd80cfe
Shashank Rajesh <shashank@Shashanks-MacBook-Pro.local>
2019-10-29 21:06:44 -0400

    Updated implementation for Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-import Navbar from './components/Navbar/Navbar.js'
-import NavbarItem from './components/Navbar/NavbarItem.js'
+import Navbar from "./components/Navbar/Navbar.js";
+import NavbarItem from "./components/Navbar/NavbarItem.js";
+
+import accountIcon from "./assets/account-circle.svg";
+import dashboardIcon from "./assets/dashboard.svg";
 
 const Viewport = styled.div`
-  height: 100%;
-  width: 100%;
-  background-color: #313131;
+	height: 100%;
+	width: 100%;
+	background-color: #313131;
 `;
 
 const App = () => {
-  return <Viewport>
-    <Navbar>
-      <NavbarItem />
-      <NavbarItem />
-    </Navbar>
-  </Viewport>
-}
+	return (
+		<Viewport>
+			<Navbar onChange={val => console.log(val)}>
+				<NavbarItem icon={accountIcon} />
+				<NavbarItem icon={dashboardIcon} />
+			</Navbar>
+		</Viewport>
+	);
+};
 
 export default App;
