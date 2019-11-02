ff1a4886dfad908258c044a1c9af8faef6e25f4f
Shashank Rajesh <shashank@Shashanks-MacBook-Pro.local>
2019-11-01 21:13:51 -0400

    Improvements

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,17 @@
 import React, { useState, useCallback, useEffect } from "react";
 import styled from "styled-components";
 import { animated, useSpring, config } from "react-spring";
+import CONSTANTS from '../../CONSTANTS.js'
 
 const Bar = styled.div`
-	width: 97px;
+	display: inline-block;
+	width: 110px;
 	height: calc(100% - 20px);
+	/* height: 100%; */
 	/* TODO: Find a different solution from using transforms in order to prevent animation conflicts */
-	transform: translateX(10px) translateY(10px);
-	background-color: #5A5A5A;
+	/* transform: translateX(10px) translateY(10px); */
+	margin: 10px 0 10px 10px;
+	background-color: ${CONSTANTS.COLOR_1};
 	border-radius: 21px;
 	box-sizing: border-box;
 	display: flex;
@@ -17,13 +21,13 @@ const Bar = styled.div`
 
 const Selector = styled.div`
 	height: calc(100% / ${props => props.count} - 22px);
-	width: calc(100% - 22px);
-	margin: 11px;
+	width: calc(100% - 20px);
+	margin: 10px;
 	position: absolute;
-	background-color: #818181;
+	background-color: ${CONSTANTS.COLOR_2};
 	border-radius: 15px;
 	pointer-events: none;
-	z-index: -100;
+	z-index: 0;
 `;
 const AnimatedSelector = animated(Selector);
 
@@ -32,6 +36,7 @@ const ItemWrapper = styled.div`
 	background-clip: content-box;
 	flex-grow: 1;
 	position: relative;
+	z-index: 1;
 `;
 
 // TODO: Implement logic to update when window is resized.
@@ -39,7 +44,7 @@ const Navbar = props => {
 	const [selection, setSelection] = useState(0);
 	const [stride, setStride] = useState(null);
 
-	useEffect(() => props.onChange(selection),[selection])
+	useEffect(() => props.onChange(selection), [selection]);
 
 	const measuredRef = useCallback(node => {
 		if (node !== null) {
@@ -52,7 +57,7 @@ const Navbar = props => {
 	const SelectorSpring = useSpring({
 		from: { transform: "translateY(0px)" },
 		transform: `translateY(${stride * selection})`,
-		config: { mass: 1, tension: 375, friction: 30 }
+		config: { mass: 1, tension: 400, friction: 30 }
 	});
 
 	return (
