0a854b9c6abb698633323cfe4d443e724a0a344f
Shashank Rajesh <shashank@Shashanks-MacBook-Pro.local>
2019-11-21 20:53:25 -0500

    Now works with CounterCard for the most part! Still improving.

--- src/components/CounterList/CounterList.js
+++ src/components/CounterList/CounterList.js
@@ -16,18 +16,32 @@ const Test = styled.div`
 `;
 const CounterList = props => {
 	const [counters, setCounters] = useState([]);
+	const [active, setActive] = useState(null);
+
+	// TODO replace inline styles for dynamic opacity with styled-components.
 
 	return (
 		<ScrollBox>
 			<Flipper flipKey={counters}>
 				{counters.map(counter => (
 					<Flipped key={counter} flipId={counter}>
-						<Test key={counter} />
+						{/* <Test key={counter} /> */}
+						<div
+							onClick={() => setActive(counter)}
+							style={{ opacity: counter === active || !active ? "1" : ".25" }}
+						>
+							<CounterCard key={counter} onClose={() => setActive(null)}>{counter}</CounterCard>
+						</div>
 					</Flipped>
 				))}
 
 				<Flipped flipId={"add"}>
-					<AddCard onClick={() => setCounters([shortid.generate(), ...counters])} />
+					<div>
+						<AddCard
+							style={{ opacity: !active ? "1" : ".25" }}
+							onClick={() => setCounters([...counters, shortid.generate()])}
+						/>
+					</div>
 				</Flipped>
 			</Flipper>
 		</ScrollBox>
